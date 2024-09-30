e3cc27f116ca51ce6aff46153e0c128704ded6fd
Eric Nwogbo <ericis2007@gmail.com>
2024-09-30 18:25:21 +0100

    Removed grammar.bnf

---
+++ src/interpreter/parser.cpp
@@ -0,0 +1,8 @@
+//
+// Created by eric on 9/29/24.
+//
+
+#include "parser.h"
+
+namespace cpplox {
+} // cpplox
---
+++ src/interpreter/parser.h
@@ -0,0 +1,13 @@
+//
+// Created by eric on 9/29/24.
+//
+
+#pragma once
+
+namespace cpplox {
+
+    class parser {
+    
+    };
+
+} // cpplox
